766ae356ecbe43000b28d1d85ec52cf6d4eec911
mission-Bell <due.missionbells@gmail.com>
2024-04-28 22:05:31 +0000

    初回アップロード、openapi.yml作成

---
+++ ccx/src/main/java/com/hyomoto/ccx/CcxApplication.java
@@ -0,0 +1,13 @@
+package com.hyomoto.ccx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcxApplication.class, args);
+	}
+
+}
---
+++ ccx/src/test/java/com/hyomoto/ccx/CcxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyomoto.ccx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
